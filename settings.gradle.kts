312361eec661aded8adb94fabcb4118c920558e6
tamzi <3008932+tamzi@users.noreply.github.com>
2025-04-26 23:53:30 +0200

    add new services

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,13 @@
 rootProject.name = "kosprin"
 
-
+listOf(
+    "analytics-service",
+    "common",
+    "feed-service",
+    "frontend-dashboard",
+    "gateway",
+    "metadata-service",
+    "notification-service",
+    "search-service",
+    "video-service"
+).forEach { include(it) }
